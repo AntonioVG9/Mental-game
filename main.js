858d89aa03e51d22748ae71477ef6eb9a93da184
AntonioVG9 <antoniovalle.huelva@gmail.com>
2026-02-03 19:16:08 +0100

    Update

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+/*ANIMACIÓN DEL BOTÓN DEL NAVEGADOR*/ 
 const toggleBtn = document.getElementById("menuToggle");
 const navLinks = document.getElementById("navLinks");
 
@@ -11,3 +11,40 @@ navLinks.querySelectorAll("a").forEach(link => {
     navLinks.classList.remove("open");
   });
 });
+
+
+
+/*BACKEND ENVÍO DE CORREOS*/ 
+
+const form = document.getElementById("contactForm");
+
+if (form) {
+  form.addEventListener("submit", async function (e) {
+    e.preventDefault();
+
+    const data = {
+      name: form.elements["name"].value,
+      email: form.elements["email"].value,
+      message: form.elements["message"].value,
+    };
+
+    try {
+      const res = await fetch("http://localhost:3000/contact", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      });
+
+      if (res.ok) {
+        alert("Mensaje enviado correctamente");
+        form.reset();
+      } else {
+        alert("Error al enviar el mensaje");
+      }
+    } catch (error) {
+      console.error(error);
+      alert("No se pudo conectar con el servidor");
+    }
+  });
+}
+
